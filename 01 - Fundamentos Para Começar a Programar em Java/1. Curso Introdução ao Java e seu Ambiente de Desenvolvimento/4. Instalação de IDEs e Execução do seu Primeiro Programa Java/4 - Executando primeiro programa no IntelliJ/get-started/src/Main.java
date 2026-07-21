a97c5eb7c20f42f8ad3b0942ab91289cfd713315
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-21 17:57:13 -0300

    4 - Executando primeiro programa no IntelliJ

---
+++ 01 - Fundamentos Para Começar a Programar em Java/1. Curso Introdução ao Java e seu Ambiente de Desenvolvimento/4. Instalação de IDEs e Execução do seu Primeiro Programa Java/4 - Executando primeiro programa no IntelliJ/get-started/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        String msg = "Programando em";
+        System.out.println(msg + args[0]);
+        System.out.println("Precisamos de uma " + args[1]);
+    }
+}
