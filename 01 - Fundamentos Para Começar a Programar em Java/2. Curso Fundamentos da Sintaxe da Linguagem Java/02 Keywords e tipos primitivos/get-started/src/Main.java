b17d5bb7ae85a5e0c0677c9ff6a349c3a3a2824c
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-22 21:33:38 -0300

    Keywords e tipos primitivos

---
+++ 01 - Fundamentos Para Começar a Programar em Java/2. Curso Fundamentos da Sintaxe da Linguagem Java/02 Keywords e tipos primitivos/get-started/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo de tipos primitivos e keywords em Java
+
+        //Declarando o byte
+        byte number = 1;
+
+        //Declarando o short
+        short number2 = 1;
+
+        //Declarando o int
+        int number3 = 1;
+
+        //Declarando o long
+        long number4 = 1L;
+        var number5 = 1L;
+
+        //Declarando o float
+        float number6 = 1.0f;
+        var number7 = 1.0f;
+
+        //Declarando o double
+        double number8 = 1.0;
+        var number9 = 1.0d;
+
+        //Declarando o char
+        char character = 'a';
+        var character2 = 'a';
+
+        //Declarando o boolean
+        boolean bool = true;
+        boolean bool2 = true;
+        var bool3 = true;
+        var bool4 = false;
+    }
+
+}
